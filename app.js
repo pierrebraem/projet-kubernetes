37f76819241ab540d1fc38b22efeb1802671bad9
Pierre Braem <pierrebraem@orange.fr>
2024-11-07 14:59:48 +0100

    Préparation v2

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.send("hello world")
 });
 
+app.get('/getPierre', (req, res) => {
+    res.send("coucou, c'est Pierre")
+})
+
+app.get('/getJeremy', (req, res) => {
+    res.send("coucou, c'est Jérémy")
+})
+
 app.listen(appProperties.port, () => {
     console.log(`app running on port ${appProperties.port}`);
 })
